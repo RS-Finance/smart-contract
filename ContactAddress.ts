761b57c9ef4ac22d5a7ea93ce79dc1a435525ade
RS Finance Labs <52669170+RS-Finance@users.noreply.github.com>
2021-08-24 10:26:45 +0600

    Create ContactAddress.ts

---
+++ ContactAddress.ts
@@ -0,0 +1,116 @@
+const tokens = {
+  kcs: {
+    symbol: 'KCS',
+    projectLink: 'https://www.kcc.io/',
+  },
+  ksf: {
+    symbol: 'KSF',
+    address: {
+      322: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+      321: '0x755d74d009f656ca1652cbdc135e3b6abfccc455',
+    },
+    decimals: 18,
+    projectLink: 'https://KsfSwap.finance/',
+  },
+  wkcs: {
+    symbol: 'wKCS',
+    address: {
+      322: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      321: '0x4446Fc4eb47f2f6586f9fAAb68B3498F86C07521',
+    },
+    decimals: 18,
+    projectLink: 'https://KsfSwap.finance/',
+  },
+  syrup: {
+    symbol: 'SYRUP',
+    address: {
+      322: '0x009cF7bC57584b7998236eff51b98A168DceA9B0',
+      321: '0xa94d08724261978cb7d375a83173241ed49d6522',
+    },
+    decimals: 18,
+    projectLink: 'https://KsfSwap.finance/',
+  },
+  usdt: {
+    symbol: 'USDT',
+    address: {
+      322: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+      321: '0x0039f574eE5cC39bdD162E9A88e3EB1f111bAF48',
+    },
+    decimals: 18,
+    projectLink: 'https://www.paxos.com/usdt/',
+  },
+  eth: {
+    symbol: 'ETH',
+    address: {
+      322: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+      321: '0xf55af137a98607f7ed2efefa4cd2dfe70e4253b1',
+    },
+    decimals: 18,
+    projectLink: 'https://ethereum.org/en/',
+  },
+  bnb: {
+    symbol: 'BNB',
+    address: {
+      322: '0x8AC76a51cc950d9822D68b83fE1Ad321B32Cd580d',
+      321: '0x639a647fbe20b6c8ac19e48e2de44ea792c62c5c',
+    },
+    decimals: 18,
+    projectLink: '',
+  },
+  usdc: {
+    symbol: 'USDC',
+    address: {
+      322: '0x8AC76a51cc950d9822D68b83fE1Ad321B32Cd580d',
+      321: '0x980a5AfEf3D17aD98635F6C5aebCBAedEd3c3430',
+    },
+    decimals: 18,
+    projectLink: 'https://www.centre.io/usdc',
+  },
+  busd: {
+    symbol: 'BUSD',
+    address: {
+      322: '0x8AC76a51cc950d9822D68b83fE1Ad321B32Cd580d',
+      321: '0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d',
+    },
+    decimals: 18,
+    projectLink: '',
+  },
+  rs: {
+    symbol: 'RS',
+    address: {
+      322: '0x516f50028780b60e2fe08efa853124438f9e46a7',
+      321: '0x1bbd57143428452a4deb42519391a0a436481c8e',
+    },
+    decimals: 18,
+    projectLink: '',
+  },
+  kafe: {
+    symbol: 'KAFE',
+    address: {
+      322: '0x516f50028780b60e2fe08efa853124438f9e46a7',
+      321: '0x516f50028780b60e2fe08efa853124438f9e46a7',
+    },
+    decimals: 18,
+    projectLink: '',
+  },
+  koffee: {
+    symbol: 'KOFFEE',
+    address: {
+      322: '0x516f50028780b60e2fe08efa853124438f9e46a7',
+      321: '0xc0ffeE0000921eB8DD7d506d4dE8D5B79b856157',
+    },
+    decimals: 18,
+    projectLink: 'https://koffeswap.exchange',
+  },
+  kus: {
+    symbol: 'KUS',
+    address: {
+      322: '0x516f50028780b60e2fe08efa853124438f9e46a7',
+      321: '0x4a81704d8c16d9fb0d7f61b747d0b5a272badf14',
+    },
+    decimals: 18,
+    projectLink: 'https://kuswap.io',
+  },
+}
+
+export default tokens
